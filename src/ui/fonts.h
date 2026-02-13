db4fc03103f09a07f6f4eb1bdcc027c33da7ff68
Icarus_rafael <gdblzx2005@163.com>
2026-02-13 09:54:33 +0800

    新字体我传一下

--- src/ui/fonts.h
+++ src/ui/fonts.h
@@ -10,6 +10,7 @@ extern "C" {
 extern const lv_font_t ui_font_sourcesans_reg_14;
 extern const lv_font_t ui_font_sourceselif_heavy_24;
 extern const lv_font_t ui_font_bebas_40;
+extern const lv_font_t ui_font_bebas_bold_10;
 extern const lv_font_t ui_font_bebas_bold_72;
 extern const lv_font_t ui_font_fontawesome;
 
